74fb06be12f23558c99de9bfef3aa8b1f3268ec2
Gianfranco Bennaser <gbennaser@danielcastelao.org>
2020-10-20 10:55:05 +0200

    boletin2.2

---
+++ boletin2.2/Boletin22.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2.pkg2;
+
+/**
+ *
+ * @author gianf
+ */
+public class Boletin22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       float lado = 3;
+       float area;
+        area=lado*lado;
+        
+        System.out.println("area="+lado*lado);
+        
+    }
+    
+}
